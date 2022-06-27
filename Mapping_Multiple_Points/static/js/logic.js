bf4465784c2521cb3c1b4f66a3ca017194b9cf4d
Adriana Hernandez <adrianahe@lstc.lesschwab.com>
2022-06-26 18:55:42 -0700

    updates

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -12,16 +12,19 @@ let streets = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
     accessToken: API_KEY
 });
 // Get data from cities.js
-let cityData = cities;
+let citiesData = cities;
 
 // Loop through the cities array and create one marker for each city.
 citiesData.forEach(function(city) {
     console.log(city)
-    L.circleMarker(city.location)
-        radius:city.population/100000
+    L.circleMarker(city.location, {
+        radius: city.population/100000,
+        color: "orange",
+        weight: 4
     })
-    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .bindPopup(`<h2>${city.city}, ${city.state}</h2> <hr> <h3>Population ${city.population.toLocaleString()}</h3>`)
 .addTo(map);
-});
+
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+})
