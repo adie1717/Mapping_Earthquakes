b7152a92531853122f31d0e0f7e842169942eb52
Adriana Hernandez <adrianahe@lstc.lesschwab.com>
2022-06-28 17:39:35 -0700

    updates

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -2,13 +2,37 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+
+// Coordinates for each point to be used in the line.
+let line = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790],
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
+  ];
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+    color: "yellow"
+  }).addTo(map);
+
+  // Coordinates for SFO to Austin and Toronto
+let line2 = [
+  [40.641766, -73.7809685],
+  [30.197429, -97.6663058],
+  [43.677717, -79.6248196]
+];
+
+// Create a polyline using the line coordinates and make the line blue
+L.polyline(line, {
+  color: "blue"
+}).addTo(map);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+let streets = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    zoomOffset: -1,
     accessToken: API_KEY
 });
 // Then we add our 'graymap' tile layer to the map.
